9f57c83e49abf37ec6a296807ccf5b09250c6a92
Makkid <Makkid@users.noreply.github.com>
2017-10-16 09:33:25 -0700

    Added JS

---
+++ script.js
@@ -0,0 +1,12 @@
+//this function responds with a fortune
+//when click the magic 8 ball
+function shakeMagic8Ball(){
+  //alert("test");
+
+  //grabs image by id
+  document.getElementById("magic8ball").src="magic8ball1.png";
+
+  //grab the results
+  document.getElementById("results").innerHTML="Concentrate and ask again."
+
+}
