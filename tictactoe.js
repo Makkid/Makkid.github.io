043d05967176af76430103b9c7c61f650508b6cf
Makkid <Makkid@users.noreply.github.com>
2017-11-06 08:29:31 -0800

    no overwriting characters and prompting for whose turn.

--- tictactoe.js
+++ tictactoe.js
@@ -1,15 +1,22 @@
-var character = 'X';
+var character = 'O';
 
 //this function handles the onclick event
 function turn(location){
 
-  //switch between x and o
-  if(character == 'X'){
-    character = 'O';
-  }
-  else{
-    character = 'X';
+  //so a already written character doesn't get over written
+  if(document.getElementById(location).innerHTML == ""){
+    document.getElementById(location).innerHTML = character;
   }
 
-  document.getElementById(location).innerHTML = character;
+    //switch between x and o
+    if(character == 'X'){
+      character = 'O';
+    }
+    else{
+      character = 'X';
+    }
+
+    //update results div with feedback
+    document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
+
 }
