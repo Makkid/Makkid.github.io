d033ee607007b03885b0fb3b49ef88b398aad659
Makkid <Makkid@users.noreply.github.com>
2017-11-13 09:44:18 -0800

    winner declaration

--- tictactoe.js
+++ tictactoe.js
@@ -5,8 +5,9 @@ function turn(location){
 
   //so a already written character doesn't get over written
   if(document.getElementById(location).innerHTML == ""){
-    document.getElementById(location).innerHTML = character;
-  }
+
+    //update results div with feedback
+    document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
 
     //switch between x and o
     if(character == 'X'){
@@ -16,7 +17,52 @@ function turn(location){
       character = 'X';
     }
 
-    //update results div with feedback
-    document.getElementById("results").innerHTML = "It's " + character + "'s turn!";
+    document.getElementById(location).innerHTML = character;
+    //check for winner after turn
+    checkWinner();
+  }
 
+}
+
+//checks for winner
+function checkWinner(){
+
+  //grab HTML of each tic tac toe div (which holds X or O)
+  var r1c1 = document.getElementById("r1c1").innerHTML;
+  var r1c2 = document.getElementById("r1c2").innerHTML;
+  var r1c3 = document.getElementById("r1c3").innerHTML;
+  var r2c1 = document.getElementById("r2c1").innerHTML;
+  var r2c2 = document.getElementById("r2c2").innerHTML;
+  var r2c3 = document.getElementById("r2c3").innerHTML;
+  var r3c1 = document.getElementById("r3c1").innerHTML;
+  var r3c2 = document.getElementById("r3c2").innerHTML;
+  var r3c3 = document.getElementById("r3c3").innerHTML;
+
+  if(((r1c1 == r1c2) && (r1c1 == r1c3) && r1c1 != '') ||
+    ((r2c1 == r2c2) && (r2c1 == r2c3) && r2c1 != '') ||
+    ((r3c1 == r3c2) && (r3c1 == r3c3) && r3c1 != '') ||
+    ((r1c1 == r2c1) && (r1c1 == r3c1) && r1c1 != '') ||
+    ((r1c2 == r2c2) && (r2c2 == r3c2) && r1c2 != '') ||
+    ((r1c3 == r2c3) && (r1c3 == r3c3) && r1c3 != '') ||
+    ((r1c1 == r2c2) && (r1c1 == r3c3) && r1c1 != '') ||
+    ((r1c3 == r2c2) && (r3c1 == r3c1) && r1c3 != '')){
+
+    //alert("Winner Winner Chicken Dinner!");
+
+    //update results div
+    document.getElementById("results").innerHTML = character + " wins!";
+  }
+
+}
+
+function clearAll(){
+  document.getElementById("r1c1").innerHTML = "";
+  document.getElementById("r1c2").innerHTML = "";
+  document.getElementById("r1c3").innerHTML = "";
+  document.getElementById("r2c1").innerHTML = "";
+  document.getElementById("r2c2").innerHTML = "";
+  document.getElementById("r2c3").innerHTML = "";
+  document.getElementById("r3c1").innerHTML = "";
+  document.getElementById("r3c2").innerHTML = "";
+  document.getElementById("r3c3").innerHTML = "";
 }
