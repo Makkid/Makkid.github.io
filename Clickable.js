b601ba02729a49878b00be3d160571b30b54b5de
Makkid <Makkid@users.noreply.github.com>
2017-12-04 09:45:49 -0800

    Clickable project and start of gallery

--- Clickable.js
+++ Clickable.js
@@ -1,27 +1,31 @@
-var variable = x
-
-function Rannum() {
-  var x = Math.floor((Math.random() * 100) + 1);
-  document.getElementById("demo").innerHTML = x;
-}
 
 //draws a random circle in click panel
-function randomCircle() {
+function randomCircle(){
 
   //randomly get top positions
-
+  document.getElementById("circle").style.top = randomNum();
 
   //randomly get left positions
+  document.getElementById("circle").style.left = randomNum();
 
   //update position of circle
 
 }
 
-function clickCircle() {
+function clickCircle(){
 
   //circle is clicked, hide circle
-document.getElementById("circle").style.visibility = "hidden";
+  document.getElementById("circle").style.visibility = "hidden";
+
+  randomCircle();
 
   //generate random circle
+  document.getElementById("circle").style.visibility = "visible";
+
+}
+
+function randomNum(){
+
+  return Math.floor(Math.random() * 200);
 
 }
---
+++ gallery.js
@@ -0,0 +1,8 @@
+
+function previous(){
+
+}
+
+function next(){
+  
+}
